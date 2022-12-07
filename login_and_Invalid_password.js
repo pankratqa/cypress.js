0297b548b6b3fc830dd3f43f66915085942999fc
DENIS (PANKRAT) <115984236+pankratqa@users.noreply.github.com>
2022-12-07 13:09:52 +0300

    Add files via upload

---
+++ login_and_Invalid_password.js
@@ -0,0 +1,10 @@
+describe('Форма логин/не верный пароль СТАЯ', function () {
+    it('Верный логин/не верный пароль', function () {
+         cy.visit('https://staya.dog/');
+         cy.get('.header-bottom__right--link').click();
+         cy.get('.auth-form > form > [type="email"]').type('login');
+         cy.get('.auth-form > form > [type="password"]').type('pass');
+         cy.get('.auth-form__submit > .s-button__content').click();
+         cy.contains('Невозможно войти с предоставленными учетными данными.');
+     })
+ })
